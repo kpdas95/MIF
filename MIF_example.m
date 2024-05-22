d932aa8ccc443da328cd3ef7bdc7fa2f0bd98ba4
Kritiprasanna Das <64372075+kpdas95@users.noreply.github.com>
2024-05-22 10:54:45 +0530

    MIF_example.m

--- MIF_example.m
+++ MIF_example.m
@@ -4,27 +4,32 @@
 %####                                                 ####
 %#########################################################
 
-% Please cite the following paper if are using this code or
+% Please cite the following paper if you are using this code or
 % part of the code.
 %
 % [1]  Kritiprasanna Das and Ram Bilas Pachori. "Schizophrenia 
 % detection technique using multivariate iterative filtering and
 % multichannel EEG signals." Biomedical Signal Processing and 
 % Control 67 (2021): 102525.
-% [2] Antonio Cicone, Jingfang Liu, and Haomin Zhou. "Adaptive 
+% [2] Kritiprasanna Das and Ram Bilas Pachori. 
+% "Electroencephalogram based motor imagery brain computer 
+% interface using multivariate iterative filtering and spatial 
+% filtering." IEEE Transactions on Cognitive and Developmental 
+% Systems (2022).
+% [3] Antonio Cicone, Jingfang Liu, and Haomin Zhou. "Adaptive 
 % local iterative filtering for signal decomposition and 
 % instantaneous frequency analysis." Applied and Computational
 % Harmonic Analysis 41.2 (2016): 384-411.
 % 
-% For any queries or help plese feel free to write a mail to 
-% kpdas95@gmail.com. I will be hapy to help.
-%% Create random signal of 5 channels and length 1000
+% For any queries or help please feel free to write an mail to 
+% kpdas95@gmail.com. I will be happy to help.
+%% Create a random signal of 5 channels and a length of 1000
 
 x = rand(5,1000);
 opt=Settings_IF_v1('IF.Xi',2,'IF.alpha','ave','IF.delta',.001,'IF.NIMFs',20);
 MIMF = IterFiltMulti(x,opt);
 
-%% Load an multichannel EEG signal
+%% Load a multichannel EEG signal
 load('Subject00_1_edfm.mat')
 
 %% Decompose using MIF
